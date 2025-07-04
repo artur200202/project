d218838f330fec3b448cb3405841f649675aff7d
Artur <arturnazaryan@macbookpro.attlocal.net>
2025-07-04 12:58:59 -0700

    codtionals

--- hello.py
+++ hello.py
@@ -9,3 +9,30 @@ print("the sum of", a, "and", b, "is", result)
 print(type(a))
 print(type(name))
 print(type(items))
+
+
+#age = int(input("What is your age: "))
+
+#if age >= 18:
+ # print("You can vote")
+#else:
+ # print("you are not old enough")
+
+grade= 94
+if grade >= 90:
+  print("grade A")
+elif grade >=80:
+  print("grade B")
+elif grade>= 70:
+  print("grade C")
+else: 
+  print("keep trying")
+
+
+age= 25
+if age >= 18 and age<=60:
+  print('you can work')
+elif age<=18:
+  print('you are to young')
+elif age>=60:
+  print ('you are to old ')
