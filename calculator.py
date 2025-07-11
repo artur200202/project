18d08d95e247bd571d7d39b3dc9513adbe7050c7
Artur <arturnazaryan@macbookpro.attlocal.net>
2025-07-11 11:36:33 -0700

    improved version

--- calculator.py
+++ calculator.py
@@ -1,27 +1,47 @@
+#add an exit condition  to quit the program
+#add while true:loop so user can keep calcualtring unyil they type exit
+def add(a,b):
+  return a + b
+
+def subtract(a,b):
+  return a - b
+
+def multiply(a,b):
+  return a * b
+
+def divide(a,b):
+  return a / b
+
+
 print("welcome to smart calculator!")
 print("choose an option: +, -, *, /")
-operation = input("enter the operation: ")
-num1 = input("enter the first number: ")
-num2= input("enter the second number: ")
-
-try: 
-  num1 = float(num1)
-  num2 = float(num2)
-
-  if operation == "+": 
-    print("Result: ", num1 + num2 )
-  elif operation == "-":
-    print("Result: ", num1 - num2)
-  elif operation == "*":
-    print("Result: ", num1 * num2)
-  elif operation == "/":
-    if num2 != 0: 
-      print("Result: ", num1 / num2)
-    else: 
-      print("Error: cannot divide by zero.")
+while True:
+  operation = input(" what do you want to do (exit, help, history, +, -, *, /): ")
+  
+  if operation == "exit":
+    break
   else: 
-    print("Invalid operation. Use  +, -, *, /.  ")
+    
+    try: 
+      num1 = input("enter the first number: ")
+      num2= input("enter the second number: ")
+      num1 = float(num1)
+      num2 = float(num2)
 
-except ValueError:
-  print("Error: please enter valid numbers.")
+      if operation == "+": 
+        print("Result: ", add(num1, num2) )
+      elif operation == "-":
+        print("Result: ", subtract(num1, num2))
+      elif operation == "*":
+        print("Result: ", multiply(num1, num2))
+      elif operation == "/":
+        if num2 != 0: 
+          print("Result: ", divide(num1, num2))
+        else: 
+         print("Error: cannot divide by zero.")
+      else  : 
+        print("Invalid operation. Use  +, -, *, /.  ")
+    
+    except ValueError:
+      print("Error: please enter valid numbers.")
     
--- hello.py
+++ hello.py
@@ -10,13 +10,12 @@ print(type(a))
 print(type(name))
 print(type(items))
 
-
-#age = int(input("What is your age: "))
-
-#if age >= 18:
- # print("You can vote")
-#else:
- # print("you are not old enough")
+#we need users age
+#we need to know if the person is 18+
+#if age is met , then we ask if user has voting card
+#if they do have then access granted
+#if they dont have , then Id is required
+#if user is not above 18 then user is to young
 
 grade= 94
 if grade >= 90:
@@ -36,3 +35,73 @@ elif age<=18:
   print('you are to young')
 elif age>=60:
   print ('you are to old ')
+
+#find out the wheater is it sunny, rainy , or cold? use input
+#if sunny then ask if its morning or afternoon? use input
+#if its morning then wear glasses and a hat, and if its afternoon use sunscreen and stay hydarated
+#if rainy do you have umbrella ? yes no 
+#if yes great stay dry/ no , find shelter quickl;y
+#if cold stay warm
+'''
+wheather= input("Is it sunny , rainy, or cold? : ")
+
+if wheather== 'sunny':
+  ask=input("if its morning, or afternoon?:")
+  if ask == "morning" :
+    print("wear sunglasses")
+  else :
+    print("Use sunscreen and stay hydrated")
+elif wheather == "rainy":
+  ask= input('do you have an unmbrella? :')
+  if ask =='yes':
+    print("stay dry")
+  else:
+    print("find shelter quickly")
+else:
+  print("stay warm!")
+'''
+'''
+for i in range(1, 13):
+  print(12*i)
+
+j=1
+while j <= 12:
+  result = 12 * j
+  print(result)
+  j+=1
+  '''
+#ask user to enter a number
+#if user enters a valid number any number,  then display multiplcation table from 1-12
+#if its not avalid number then print an error message please enter valid number
+#if the user types an exit then stop the program
+#repeat the loop until user types exit-choose a number or write exit to quit
+'''
+while True:
+  user= input ("enter a number or enter exit to leave:")
+  if user == "exit":
+    break
+  if user.isdigit():
+    for i in range(1, 13):
+      result= int (user) *i
+      print(result)
+  else:
+    print("its not valid, enter valid number")
+  '''
+age_str="25"
+age_str= int(age_str)
+age= age_str +5
+print(age)
+
+#Convert the float 6.7 to an integer and print the result
+
+height = 6.7
+height= int(height)
+print(height)
+
+value1=0
+value="hello"
+value2= bool(value1)
+print(value2)
+value3= bool(value)
+print(value3)
+
